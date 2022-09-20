405a85b5d15b5f35919868d19cd4c18a4b7753e0
rajithacharith <rajithacharith@gmail.com>
2022-09-20 23:21:35 +0530

    Add oidc outbound service client implementation and update readme.

--- README.md
+++ README.md
@@ -1,3 +1,90 @@
 # identity-outbound-oidc-auth-service
 
-Outbound OIDC authentication microservice
+## Setting up development environment.
+
+### Prerequisite
+* Install Java SE Development Kit 11
+* Install Apache Maven 3.x.x(https://maven.apache.org/download.cgi#)
+* Get a clone from https://github.com/wso2-incubator/identity-outbound-oidc-auth-service or download the source
+
+### Setting up keystores and truststores.
+Keystores and trust stores for development are attached with the repository source.
+Since this microservice communicates with Identity Server using mTLS, certificate exchange needs to be done before the communication.
+This can be done via GUI tool, keytool explorer or keytool CLI.
+#### Exporting certificate from microservice keystore.
+Execute following command inside the `$MICROSERVICE_HOME/resources/security` directory.
+```
+keytool  -export -alias localhost \
+-keystore keystore.jks \
+-file outbound-service.crt \
+-rfc -storepass wso2carbon
+```
+Above command will generate a file named `outbound-service.crt`. Move that to `IS_HOME/repository/resources/security` directory.
+
+Execute following command inside the `IS_HOME/repository/resources/security` directory to import the certifate of the microservice to the IS truststore.
+```
+keytool -import -alias localhost -file outbound-service.crt \
+-keystore client-truststore.jks \
+-storepass wso2carbon \
+-noprompt
+```
+
+#### Exporting certificate from Identity Server
+Execute following command inside the `IS_HOME/repository/resources/security` directory to extract the IS certificate.
+
+```
+keytool  -export -alias wso2carbon \
+        -keystore wso2carbon.jks \
+        -file is.crt \
+        -rfc -storepass wso2carbon
+```
+Above command will generate a file named `is.crt`. Move that file to `$MICROSERVICE_HOME/resources/security` directory.
+
+Execute following command inside `$MICROSERVICE_HOME/resources/security` directory.
+```
+keytool -import -alias wso2carbon -file wso2carbon.crt \
+        -keystore client-truststore.jks \
+        -storepass wso2carbon \
+        -noprompt
+```
+
+### Building the project.
+Both server and client can be built using the following maven command.
+`mvn clean install` inside the `$PROJECT_ROOT` directory.
+
+This will build the jars for both server and client.
+
+### Running the microservice.
+Maven build will build the microservice as a jar which can be run as a separate service.
+The jar can be found in the `PROJECT_HOME/components/org.wso2.identity.outbound.oidc.auth.service/target` directory as
+`org.wso2.identity.outbound.oidc.auth.service-<version>-full.jar`.
+
+This jar needs the resources (keystores, properties files) to run. Copy the jar to the resources folder. 
+
+Run the microservice using the following command.
+```
+java -Dlog4j.configurationFile=log4j2.properties -jar com.wso2.identity.asgardeo.outbound.oidc.service-<version>-full.jar
+```
+
+Once the server is running following log can be seen in the terminal.
+```
+TID: [1] [2022-09-21 07:58:06,124] [] : iam-cloud-outbound-oidc-service :  INFO {org.wso2.identity.outbound.oidc.auth.service.OutboundOIDCServer} - Outbound OIDC Service started successfully. Listening on port 5002
+```
+
+### Setting up Identity Server.
+
+The microservice client jar can be found in `PROJECT_HOME/components/org.wso2.identity.outbound.oidc.auth.client/target` directory as `org.wso2.idenitity.outbound.oidc.auth.client-<version>.jar`
+
+Copy that jar file to `IS_HOME/repository/components/dropins` directory.
+
+Add following configuration to `deployment.toml` file in IS.
+```
+[authentication.authenticator.microservice]
+name = "MicroServiceAuthenticator"
+enable = true
+parameters.url = "localhost:5002"
+```
+
+* Start the Identity Server and set up an Identity Provider with `Microservice-Authenticator` as the Federated Authenticator.
+* Setup a Service Provider and use newly created Identity Provider in `Local & Outbound Authentication Configuration`.
+* Try logging into the configured Service Provider.
---
+++ components/org.wso2.identity.outbound.oidc.auth.client/src/main/java/org/wso2/identity/outbound/oidc/auth/client/CustomOIDCAuthenticator.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.identity.outbound.oidc.auth.client;
+
+import com.nimbusds.jose.util.JSONObjectUtils;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
+import org.apache.oltu.oauth2.client.response.OAuthClientResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.wso2.carbon.identity.application.authentication.framework.AbstractApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.FederatedApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
+import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
+import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
+import org.wso2.carbon.identity.application.common.model.Property;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
+import org.wso2.identity.outbound.oidc.auth.client.constants.CustomOIDCAuthenticatorConstants;
+import org.wso2.identity.outbound.oidc.auth.service.rpc.InitAuthRequest;
+import org.wso2.identity.outbound.oidc.auth.service.rpc.InitAuthResponse;
+import org.wso2.identity.outbound.oidc.auth.service.rpc.Request;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * CustomOIDC Authenticator to connect to the external outbound oidc microservice.
+ */
+public class CustomOIDCAuthenticator extends AbstractApplicationAuthenticator
+        implements FederatedApplicationAuthenticator {
+
+    private static final Log log = LogFactory.getLog(CustomOIDCAuthenticator.class);
+    private OIDCOutboundClient oidcOutboundClient;
+
+    public CustomOIDCAuthenticator() {
+
+        this.oidcOutboundClient = new OIDCOutboundClient();
+    }
+
+    @Override
+    public boolean canHandle(HttpServletRequest request) {
+
+        log.debug("Invoking canHandle method in remote microservice.");
+        Request req = Request.newBuilder()
+                .setRequestURL(request.getRequestURI())
+                .setQueryString(request.getQueryString())
+                .addRequestParams(
+                        Request.RequestParam.newBuilder()
+                        .setParamName(CustomOIDCAuthenticatorConstants.OAUTH2_PARAM_STATE)
+                        .addParamValue(getRequestState(request)).build())
+                .build();
+
+        return oidcOutboundClient.canHandle(req, getAuthenticatorConfig().getParameterMap().get("url"));
+    }
+
+    @Override
+    public String getFriendlyName() {
+
+        return "microservice-authenticator";
+    }
+
+    @Override
+    public String getName() {
+
+        return "MicroServiceAuthenticator";
+    }
+
+    @Override
+    public String getClaimDialectURI() {
+
+        // Get the claim dialect URI if this authenticator receives claims in a standard dialect.
+        return CustomOIDCAuthenticatorConstants.OIDC_DIALECT;
+    }
+
+    @Override
+    public List<Property> getConfigurationProperties() {
+
+        // Get the required configuration properties.
+        List<Property> configProperties = new ArrayList<>();
+        Property clientId = new Property();
+        clientId.setName(CustomOIDCAuthenticatorConstants.CLIENT_ID);
+        clientId.setDisplayName("Client Id");
+        clientId.setRequired(true);
+        clientId.setDescription("Enter OAuth2/OpenID Connect client identifier value");
+        clientId.setType("string");
+        clientId.setDisplayOrder(1);
+        configProperties.add(clientId);
+
+        Property clientSecret = new Property();
+        clientSecret.setName(CustomOIDCAuthenticatorConstants.CLIENT_SECRET);
+        clientSecret.setDisplayName("Client Secret");
+        clientSecret.setRequired(true);
+        clientSecret.setDescription("Enter OAuth2/OpenID Connect client secret value");
+        clientSecret.setType("string");
+        clientSecret.setDisplayOrder(2);
+        clientSecret.setConfidential(true);
+        configProperties.add(clientSecret);
+
+        Property callbackUrl = new Property();
+        callbackUrl.setDisplayName("Callback URL");
+        callbackUrl.setName(CustomOIDCAuthenticatorConstants.CALLBACK_URL);
+        callbackUrl.setDescription("The callback URL used to partner identity provider credentials.");
+        callbackUrl.setDisplayOrder(3);
+        configProperties.add(callbackUrl);
+
+        Property authzEpUrl = new Property();
+        authzEpUrl.setName(CustomOIDCAuthenticatorConstants.OAUTH2_AUTHZ_URL);
+        authzEpUrl.setDisplayName("Authorization Endpoint URL");
+        authzEpUrl.setRequired(true);
+        authzEpUrl.setDescription("Enter OAuth2/OpenID Connect authorization endpoint URL value");
+        authzEpUrl.setType("string");
+        authzEpUrl.setDisplayOrder(4);
+        configProperties.add(authzEpUrl);
+
+        Property tokenEpUrl = new Property();
+        tokenEpUrl.setName(CustomOIDCAuthenticatorConstants.OAUTH2_TOKEN_URL);
+        tokenEpUrl.setDisplayName("Token Endpoint URL");
+        tokenEpUrl.setRequired(true);
+        tokenEpUrl.setDescription("Enter OAuth2/OpenID Connect token endpoint URL value");
+        tokenEpUrl.setType("string");
+        tokenEpUrl.setDisplayOrder(5);
+        configProperties.add(tokenEpUrl);
+
+        return configProperties;
+    }
+
+    @Override
+    protected void initiateAuthenticationRequest(HttpServletRequest request, HttpServletResponse response,
+                                                 AuthenticationContext context) throws AuthenticationFailedException {
+
+        try {
+            Map<String, String> authenticatorProperties = context.getAuthenticatorProperties();
+            org.wso2.identity.outbound.oidc.auth.service.rpc.AuthenticationContext authenticationContext
+                    = org.wso2.identity.outbound.oidc.auth.service.rpc.AuthenticationContext.newBuilder()
+                    .putAllAuthenticatorProperties(authenticatorProperties)
+                    .setContextIdentifier(context.getContextIdentifier())
+                    .putAllAuthenticatorParams(context.getAuthenticatorParams(context.getCurrentAuthenticator()))
+                    .build();
+
+            InitAuthRequest initAuthRequest = InitAuthRequest.newBuilder()
+                    .setAuthenticationContext(authenticationContext).build();
+
+            InitAuthResponse initAuthResponse = oidcOutboundClient.initiateAuthenticationRequest(initAuthRequest,
+                    getAuthenticatorConfig().getParameterMap().get("url"));
+            if (initAuthResponse != null && initAuthResponse.getIsRedirect()) {
+                response.sendRedirect(initAuthResponse.getRedirectUrl());
+            } else {
+                throw new AuthenticationFailedException("Exception while handling initiateAuthenticationRequest.");
+            }
+        } catch (IOException e) {
+            throw new AuthenticationFailedException("Exception while building authorization code request", e);
+        }
+    }
+
+    @Override
+    protected void processAuthenticationResponse(HttpServletRequest request, HttpServletResponse response,
+                                                 AuthenticationContext context) throws AuthenticationFailedException {
+
+        try {
+            /* Process authentication response to get the id token to set the subject to the
+             Authentication context from the sub claim value. */
+            OAuthAuthzResponse authzResponse = OAuthAuthzResponse.oauthCodeAuthzResponse(request);
+            OAuthClientRequest accessTokenRequest = getAccessTokenRequest(context, authzResponse);
+            OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+            OAuthClientResponse oAuthResponse = getOauthResponse(oAuthClient, accessTokenRequest);
+            String accessToken = oAuthResponse.getParam(CustomOIDCAuthenticatorConstants.ACCESS_TOKEN);
+
+            if (StringUtils.isBlank(accessToken)) {
+                throw new AuthenticationFailedException("Access token is empty or null");
+            }
+
+            String idToken = oAuthResponse.getParam(CustomOIDCAuthenticatorConstants.ACCESS_TOKEN);
+            if (StringUtils.isBlank(idToken)) {
+                throw new AuthenticationFailedException("Id token is required and is missing in OIDC response");
+            }
+
+            context.setProperty(CustomOIDCAuthenticatorConstants.ACCESS_TOKEN, accessToken);
+
+            AuthenticatedUser authenticatedUser;
+            Map<String, Object> jsonObject = new HashMap<>();
+
+            if (StringUtils.isNotBlank(idToken)) {
+                jsonObject = getIdTokenClaims(context, idToken);
+                String authenticatedUserId = getAuthenticateUser(jsonObject);
+                if (authenticatedUserId == null) {
+                    throw new AuthenticationFailedException("Cannot find the userId from the id_token sent " +
+                            "by the federated IDP.");
+                }
+                authenticatedUser = AuthenticatedUser
+                        .createFederateAuthenticatedUserFromSubjectIdentifier(authenticatedUserId);
+            } else {
+                authenticatedUser = AuthenticatedUser.createFederateAuthenticatedUserFromSubjectIdentifier(
+                        getAuthenticateUser(jsonObject));
+            }
+            context.setSubject(authenticatedUser);
+        } catch (OAuthProblemException e) {
+            throw new AuthenticationFailedException("Authentication process failed", e);
+        }
+    }
+
+    @Override
+    public String getContextIdentifier(HttpServletRequest request) {
+
+        String state = request.getParameter(CustomOIDCAuthenticatorConstants.OAUTH2_PARAM_STATE);
+        if (state != null) {
+            return state.split(",")[0];
+        } else {
+            return null;
+        }
+    }
+
+    private String getAuthenticateUser(Map<String, Object> oidcClaims) {
+
+        // Get the authenticated user's user Id from the id_token by the sub claim value.
+        return (String) oidcClaims.get(CustomOIDCAuthenticatorConstants.SUB);
+    }
+
+    private Map<String, Object> getIdTokenClaims(AuthenticationContext context, String idToken) {
+
+        context.setProperty(CustomOIDCAuthenticatorConstants.ID_TOKEN, idToken);
+        String base64Body = idToken.split("\\.")[1];
+        byte[] decoded = Base64.decodeBase64(base64Body.getBytes(StandardCharsets.UTF_8));
+        Set<Map.Entry<String, Object>> jwtAttributeSet = new HashSet<>();
+        try {
+            jwtAttributeSet = JSONObjectUtils.parse(new String(decoded, StandardCharsets.UTF_8)).entrySet();
+        } catch (ParseException e) {
+            log.error("Error occurred while parsing JWT provided by federated IDP: ", e);
+        }
+        Map<String, Object> jwtAttributeMap = new HashMap<>();
+        for (Map.Entry<String, Object> entry : jwtAttributeSet) {
+            jwtAttributeMap.put(entry.getKey(), entry.getValue());
+        }
+        return jwtAttributeMap;
+    }
+
+    private OAuthClientRequest getAccessTokenRequest(AuthenticationContext context, OAuthAuthzResponse
+            authzResponse) throws AuthenticationFailedException {
+
+        // Extract the authentication properties from the context.
+        Map<String, String> authenticatorProperties = context.getAuthenticatorProperties();
+        String clientId = authenticatorProperties.get(CustomOIDCAuthenticatorConstants.CLIENT_ID);
+        String clientSecret = authenticatorProperties.get(CustomOIDCAuthenticatorConstants.CLIENT_SECRET);
+        String tokenEndPoint = authenticatorProperties.get(CustomOIDCAuthenticatorConstants.OAUTH2_TOKEN_URL);
+        String callbackUrl = authenticatorProperties.get(CustomOIDCAuthenticatorConstants.CALLBACK_URL);
+
+        OAuthClientRequest accessTokenRequest;
+        try {
+            // Build access token request
+            accessTokenRequest = OAuthClientRequest.tokenLocation(tokenEndPoint).setGrantType(GrantType
+                    .AUTHORIZATION_CODE).setClientId(clientId).setClientSecret(clientSecret).setRedirectURI
+                    (callbackUrl).setCode(authzResponse.getCode()).buildBodyMessage();
+            if (accessTokenRequest != null) {
+                String serviceUrl = IdentityUtil.getServicePath();
+                String serverURL = IdentityUtil.getServerURL(serviceUrl, true, true);
+                accessTokenRequest.addHeader(CustomOIDCAuthenticatorConstants.HTTP_ORIGIN_HEADER, serverURL);
+            }
+        } catch (OAuthSystemException e) {
+            throw new AuthenticationFailedException("Error while building access token request", e);
+        }
+        return accessTokenRequest;
+    }
+
+    private OAuthClientResponse getOauthResponse(OAuthClient oAuthClient, OAuthClientRequest accessRequest)
+            throws AuthenticationFailedException {
+
+        OAuthClientResponse oAuthResponse;
+        try {
+            oAuthResponse = oAuthClient.accessToken(accessRequest);
+        } catch (OAuthSystemException | OAuthProblemException e) {
+            throw new AuthenticationFailedException("Exception while requesting access token");
+        }
+        return oAuthResponse;
+    }
+
+    private String getRequestState(HttpServletRequest request) {
+
+        // Return EMPTY string if the state parameter in the request is null.
+        String state = request.getParameter(CustomOIDCAuthenticatorConstants.OAUTH2_PARAM_STATE);
+        if (StringUtils.isNotEmpty(state)) {
+            return state;
+        }
+        return StringUtils.EMPTY;
+    }
+}
